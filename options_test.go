18dc088d3d15287da4f6c4a9b05c1b3fca79486a
Christophe Lambin <christophe.lambin@gmail.com>
2024-03-21 15:15:38 +0100

    refactor: ease rebase

--- options_test.go
+++ options_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	grafanaJSONServer "github.com/clambin/grafana-json-server"
+	gjson "github.com/clambin/grafana-json-server"
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
 	"io"
@@ -16,7 +16,7 @@ import (
 )
 
 func TestWithHandlerFunc(t *testing.T) {
-	h := grafanaJSONServer.NewServer(grafanaJSONServer.WithHTTPHandler(http.MethodGet, "/extra", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+	h := gjson.NewServer(gjson.WithHTTPHandler(http.MethodGet, "/extra", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})))
 
@@ -28,18 +28,18 @@ func TestWithHandlerFunc(t *testing.T) {
 }
 
 func TestWithPrometheusQueryMetrics(t *testing.T) {
-	metrics := grafanaJSONServer.NewDefaultPrometheusQueryMetrics("namespace", "subsystem", "test")
-	handler := grafanaJSONServer.NewServer(
-		grafanaJSONServer.WithPrometheusQueryMetrics(metrics),
-		grafanaJSONServer.WithHandler("foo", grafanaJSONServer.HandlerFunc(func(_ context.Context, target string, _ grafanaJSONServer.QueryRequest) (grafanaJSONServer.QueryResponse, error) {
-			return grafanaJSONServer.TimeSeriesResponse{
+	metrics := gjson.NewDefaultPrometheusQueryMetrics("namespace", "subsystem", "test")
+	handler := gjson.NewServer(
+		gjson.WithPrometheusQueryMetrics(metrics),
+		gjson.WithHandler("foo", gjson.HandlerFunc(func(_ context.Context, target string, _ gjson.QueryRequest) (gjson.QueryResponse, error) {
+			return gjson.TimeSeriesResponse{
 				Target: target,
-				DataPoints: []grafanaJSONServer.DataPoint{
+				DataPoints: []gjson.DataPoint{
 					{Timestamp: time.Date(2023, time.July, 15, 0, 0, 0, 0, time.UTC), Value: 10},
 				},
 			}, nil
 		})),
-		grafanaJSONServer.WithHandler("fubar", grafanaJSONServer.HandlerFunc(func(_ context.Context, target string, _ grafanaJSONServer.QueryRequest) (grafanaJSONServer.QueryResponse, error) {
+		gjson.WithHandler("fubar", gjson.HandlerFunc(func(_ context.Context, target string, _ gjson.QueryRequest) (gjson.QueryResponse, error) {
 			return nil, errors.New("failed")
 		})),
 	)
@@ -76,21 +76,21 @@ namespace_subsystem_json_query_error_count{application="test",target="missing"} 
 }
 
 func TestServer_WithVariable(t *testing.T) {
-	h := grafanaJSONServer.NewServer(
-		grafanaJSONServer.WithVariable("foo", func(_ grafanaJSONServer.VariableRequest) ([]grafanaJSONServer.Variable, error) {
-			return []grafanaJSONServer.Variable{{Text: "Foo", Value: "foo"}, {Text: "Bar", Value: "bar"}}, nil
+	h := gjson.NewServer(
+		gjson.WithVariable("foo", func(_ gjson.VariableRequest) ([]gjson.Variable, error) {
+			return []gjson.Variable{{Text: "Foo", Value: "foo"}, {Text: "Bar", Value: "bar"}}, nil
 		}),
-		grafanaJSONServer.WithVariable("fubar", func(_ grafanaJSONServer.VariableRequest) ([]grafanaJSONServer.Variable, error) {
+		gjson.WithVariable("fubar", func(_ gjson.VariableRequest) ([]gjson.Variable, error) {
 			return nil, errors.New("failed")
 		}),
-		grafanaJSONServer.WithVariable("", func(request grafanaJSONServer.VariableRequest) ([]grafanaJSONServer.Variable, error) {
+		gjson.WithVariable("", func(request gjson.VariableRequest) ([]gjson.Variable, error) {
 			var p map[string]any
-			var vars []grafanaJSONServer.Variable
+			var vars []gjson.Variable
 			if err := json.Unmarshal(request.Payload, &p); err == nil {
 				for k, v := range p {
 					vars = append(vars,
-						grafanaJSONServer.Variable{Text: strings.ToTitle(k), Value: k},
-						grafanaJSONServer.Variable{Text: strings.ToTitle(v.(string)), Value: v.(string)},
+						gjson.Variable{Text: strings.ToTitle(k), Value: k},
+						gjson.Variable{Text: strings.ToTitle(v.(string)), Value: v.(string)},
 					)
 				}
 			}
